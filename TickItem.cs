23eaad15266223a909117909a3f603d818ba8999
siascone <spencer.iascone@gmail.com>
2023-04-06 16:33:43 -0600

    Start TickList Blazor App

---
+++ TickItem.cs
@@ -0,0 +1,12 @@
+public class TickItem 
+{
+    public string? Crag { get; set; }
+    public string? Route { get; set; }
+    public string? Rating { get; set; }
+    public bool? OnSite { get; set; }
+    public bool? Send { get; set; }
+    public int? NumFalls { get; set; }
+
+    public int? NumAttempts { get; set; }
+    public int? NumSends { get; set; }
+}
